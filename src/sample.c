548ed9e096b2fd0ae89b2900bfd1149c23a9f571
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2020-08-19 16:34:57 +0200

    sample creates now files during fake simulation

--- src/sample.c
+++ src/sample.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define DATA_SIZE 100
 
 int
 main (void)
 {
   printf ("Hello, world!\n");
+
+  char myStrings[][10] = { "analysis_work", "bin_work", "config_work", "couple_work", "forcing_work", "input_work", "log_work", "mon_work", "outdata_work", "restart_work", "scripts_work", "unknown_work", "viz_work", "work_work"};
+
+  size_t i = 0;
+  for( i = 0; i < sizeof(myStrings) / sizeof(myStrings[0]); i++)
+  {
+    FILE * fPtr;
+    fPtr = fopen(myStrings[i], "w");
+    if(fPtr == NULL)
+    {
+      printf("Unable to create file.\n");
+      exit(EXIT_FAILURE);
+    }
+  }
   return 0;
 }
 
